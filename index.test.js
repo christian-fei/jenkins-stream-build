10bd8a3b2ba58d0db669e42a4d1afa3aff16953e
Christian Fei <crifei93@gmail.com>
2019-09-04 00:10:51 +0200

    nock test jenkinsStreamBuild

--- index.test.js
+++ index.test.js
@@ -1,5 +1,6 @@
 const test = require('ava')
-const { buildUrl } = require('.')
+const { buildUrl, jenkinsStreamBuild } = require('.')
+const nock = require('nock')
 
 test('returns url with username, password and start parameter', async t => {
   const host = 'jenkins.example.com'
@@ -30,4 +31,23 @@ test('throws when missing username, password, host or job', async t => {
   const start = 100
 
   t.throws(() => buildUrl({ build, start }))
+})
+
+test('calls correct url based on parameters', async t => {
+  const host = 'jenkins.example.com'
+  const job = 'test-job'
+  const build = '666'
+  const username = 'test'
+  const password = 'pass'
+  const start = 0
+
+  const request = nock(`https://${username}:${password}@${host}`)
+    .get(`/job/${job}/${build}/logText/progressiveText?start=${start}`)
+    .reply(200, '', {
+      'x-more-data': 'false'
+    })
+
+  await jenkinsStreamBuild({ host, job, build, username, password })
+
+  t.notThrows(() => request.done(), 'expected url was not called')
 })
